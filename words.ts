0755532301595860a43a02b092eedc4b4f06255c
Erika-210 <erika.epp210@gmail.com>
2024-11-22 21:11:12 +0900

    Add function getRandomWord

--- words.ts
+++ words.ts
@@ -1,1 +1,7 @@
 const words: string[] = ['grape', 'peach', 'youth', 'zebra', 'chair', 'music', 'light', 'house', 'table']
+
+function getRandomWord() {
+ return words[Math.floor(Math.random() * 9)]
+}
+
+
