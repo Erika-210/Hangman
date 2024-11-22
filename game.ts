ac4adcc16f9448e3535a19987019767a27b2599e
TJ Epperson <tj.epperson070@gmail.com>
2024-11-22 21:23:07 +0900

    Add dev comments, loop skeleton

--- game.ts
+++ game.ts
@@ -1,7 +1,16 @@
 import { prompt } from "./inputHelper"
 
 const main = async () => {
+    // Set the word (import the getRandomWord function)
 
+
+    // Initialize game state (misses, guessed letters, etc.)
+
+
+    // Game loop (prompt for letter, validate input, check word for letter, etc.)
+    while (true /* replace with checks for game over / win condition */) {
+
+    }
 }
 
 main()
